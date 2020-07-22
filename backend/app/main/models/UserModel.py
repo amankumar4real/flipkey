8fdf90fbaa4bdcd49253887015f69cb1f254f4c4
Aman Kumar <amankumar4real@gmail.com>
2020-07-22 17:45:35 +0530

    added registration for backend

--- backend/app/main/models/UserModel.py
+++ backend/app/main/models/UserModel.py
@@ -6,4 +6,6 @@ class UserModel(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(60))
     email = db.Column(db.String(60),unique = True)
+    phone = db.Column(db.Integer)
     password = db.Column(db.String(60))
+    type = db.Column(db.String(60))
--- backend/app/main/services/user.py
+++ backend/app/main/services/user.py
@@ -6,22 +6,24 @@ from instance.config import SECRET_KEY
 def register(details):
     name = details["name"]
     email = details["email"]
+    phone = details["phone"]
     password = details["password"]
 
-    # available_or_not = UserModel.query.filter(UserModel.email == email).first()
+    available_or_not = UserModel.query.filter(UserModel.email == email).first()
 
-    # if available_or_not is None:
-        
-    user = UserModel(name=name, email=email, password=password)
+    if available_or_not is None:
+        user_type = "user"
 
-    db.session.add(user)
-    db.session.commit()
+        user = UserModel(name=name, email=email, phone=phone, password=password, type=user_type)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return json.dumps({
+            "error": False, "message": "Data added to the user table!"
+        })
+
 
     return json.dumps({
-        "error": False, "message": "Data added to the user table!"
-    })
-
-
-    # return json.dumps({
-    #         "error": True, "message": "Data not added to the user table!"
-    #     })
+            "error": True, "message": "Data not added to the user table!"
+        })
