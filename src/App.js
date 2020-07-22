7209b44433ab4a1f5b400643f0f9b793e55b6210
Aman Kumar <amankumar4real@gmail.com>
2020-07-23 01:52:24 +0530

    Added search feature for both home and result page!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Login from './Compoenents/Auth/Login';
+import Search from './Compoenents/common/Search'
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <Search/>
     </div>
   );
 }
