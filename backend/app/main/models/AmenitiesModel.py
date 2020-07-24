0c04155195b0ce8415050fd66fc6991988e289fe
Aman Kumar <amankumar4real@gmail.com>
2020-07-24 19:34:41 +0530

    ADDED: All the listing tables build

---
+++ backend/app/main/models/AmenitiesModel.py
@@ -0,0 +1,28 @@
+from . import db
+
+
+class AmenitiesModel(db.Model):
+    __tablename__="amenities"
+    id = db.Column(db.Integer,primary_key = True)
+    property_id = db.Column(db.Integer, db.ForeignKey("product.id"),unique = True)
+    balcony = db.Column(db.String(10))
+    staffed = db.Column(db.String(10))
+    tv = db.Column(db.String(10))
+    fans = db.Column(db.String(10))
+    linen = db.Column(db.String(10))
+    towels = db.Column(db.String(10))
+    housekeeper = db.Column(db.String(10))
+    wifi = db.Column(db.String(10))
+    swimming_pool = db.Column(db.String(10))
+    heated_pool = db.Column(db.String(10))
+    washing_machine = db.Column(db.String(10))
+    garden = db.Column(db.String(10))
+    dvd = db.Column(db.String(10))
+    grill = db.Column(db.String(10))
+    fireplace = db.Column(db.String(10))
+    cot = db.Column(db.String(10))
+    dishwash = db.Column(db.String(10))
+    microwave = db.Column(db.String(10))
+    freezer = db.Column(db.String(10))
+    game = db.Column(db.String(10))
+    sea_view = db.Column(db.String(10))
---
+++ backend/app/main/models/KeyFieldModel.py
@@ -0,0 +1,12 @@
+from . import db
+
+# child_friendly, car, ac, pets, near_beach
+class KeyFieldModel(db.Model):
+    __tablename__="key_field"
+    id = db.Column(db.Integer,primary_key = True)
+    property_id = db.Column(db.Integer, db.ForeignKey("product.id"),unique = True)
+    child = db.Column(db.String(10))
+    car = db.Column(db.String(10))
+    ac = db.Column(db.String(10))
+    pet = db.Column(db.String(10))
+    beach = db.Column(db.String(10))
