29b2ce019c64f200cbe8b80b05e806af4c10314b
Aman Kumar <amankumar4real@gmail.com>
2020-08-09 12:08:57 +0530

    changes to instance

--- backend/instance/config.py
+++ backend/instance/config.py
@@ -1,6 +1,4 @@
 SECRET_KEY = 'Hiaman@1234'
 # SQLALCHEMY_DATABASE_URI = 'mysql://root:Hiaman@1234@localhost/flipkey'
 
-SQLALCHEMY_DATABASE_URI = 'mysql://admin:Hiaman1234@aman-db.cxhv4gay8h86.ap-south-1.rds.amazonaws.com/flipkey'
-
 # JWT_KEY = "secret"
